cd36c554f44ff9e7619a6753874786cace2e592e
Clyde-fare <clyde.fare@gmail.com>
2015-01-29 17:01:42 +0000

    Changed main_g09_pbs function used by the submit_calc exist point to more informative submit_pbs_calc

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ from setuptools import setup
 setup(name='gausspy',
       version='0.1',
       packages=['gausspy', 'gausspy.io'],
+
+      entry_points = {
+        'console_scripts': [
+            'submit_calc = gausspy.g09_pbs:submit_pbs_calc',
+        ],
+        }
       )
